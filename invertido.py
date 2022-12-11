6f7bdf017054c9655bc895985c6de73a82640751
ManuelCarranzaAv <119111018+ManuelCarranzaAv@users.noreply.github.com>
2022-12-10 19:08:32 -0500

    Update invertido.py

--- invertido.py
+++ invertido.py
@@ -1,10 +1,7 @@
-# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
-# Es posible que el codigo generado no sea completamente correcto. Si encuentra
-# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
 
 
 if __name__ == '__main__':
-	print("Ingrese los productos")
+	print("Ingrese 10 productos")
 	a = str()
 	b = str()
 	c = str()
