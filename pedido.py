d62c3323058f52b19eced48f04e5e13cc77119a9
ManuelCarranzaAv <119111018+ManuelCarranzaAv@users.noreply.github.com>
2022-12-10 19:06:19 -0500

    Update pedido.py

--- pedido.py
+++ pedido.py
@@ -1,15 +1,3 @@
-# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
-# Es posible que el codigo generado no sea completamente correcto. Si encuentra
-# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
-
-
-# En python no hay forma de elegir como pasar una variable a una
-# funcion (por referencia o por valor). Las variables "inmutables"
-# (str, int, float, bool) se pasan siempre por copia mientras que
-# las demas (listas, objetos, etc.) se pasan siempre por referencia.
-# Esto coincide con el comportamiento por defecto en pseint, pero
-# cuando utiliza las palabras clave "Por Referencia" para
-# alterarlo la traducci�n no es correcta.
 
 def pedido():
 	opc = str()
@@ -22,8 +10,8 @@ def cuenta():
 	print("La cuenta es de 20 soles")
 
 if __name__ == '__main__':
-	print("Realizar pedido:")
+	print("¿Buenas tardes, ¿Que desea pedir? ")
 	pedido()
-	print("�Desea pedir la cuenta?")
+	print("¿Desea pedir la cuenta?")
 	cuenta()
 
