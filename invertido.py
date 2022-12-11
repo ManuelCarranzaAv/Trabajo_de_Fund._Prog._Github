28dc9861c1834d7302bb5d0a9f749aabd972b6c5
ManuelCarranzaAv <119111018+ManuelCarranzaAv@users.noreply.github.com>
2022-12-10 19:07:46 -0500

    Add files via upload

---
+++ invertido.py
@@ -0,0 +1,39 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print("Ingrese los productos")
+	a = str()
+	b = str()
+	c = str()
+	d = str()
+	e = str()
+	f = str()
+	g = str()
+	h = str()
+	i = str()
+	j = str()
+	a = input()
+	b = input()
+	c = input()
+	d = input()
+	e = input()
+	f = input()
+	g = input()
+	h = input()
+	i = input()
+	j = input()
+	print("-------------------------------")
+	print(j)
+	print(i)
+	print(h)
+	print(g)
+	print(f)
+	print(e)
+	print(d)
+	print(c)
+	print(b)
+	print(a)
+
